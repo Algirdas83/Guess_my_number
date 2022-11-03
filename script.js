bf346110f55f1bf65149c2ba24e43e1ac1dbd04d
Algirdas83 <algirdasliutkus@gmail.com>
2022-11-03 11:22:54 +0200

    programing done. Finis interface

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
 'use strict';
 
-const sectretNumber = Math.trunc(Math.random() * 20 + 1);
-let message = document.querySelector('.message');
-let score = 20;
-console.log(sectretNumber);
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+// const YouLost = function () {
+//   message.textContent = `You lost the game`;
+//   document.querySelector('.score').textContent = 0;
+//   document.querySelector('body').style.backgroundColor = '#E0144C';
+// };
 
-  if (!guess) {
-    message.textContent = 'No number';
-    //Whwn player wins
-  } else if (guess === sectretNumber) {
-    score++;
-    document.querySelector('.score').textContent = score;
-    message.textContent = `Correct Number`;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = `${sectretNumber}`;
-    // When user us to high
-  } else if (guess > sectretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      message.textContent = `Number to hight`;
-    } else {
-      message.textContent = `You lost the game`;
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#E0144C';
-    }
-    //When user is to low
-  } else if (guess < sectretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      message.textContent = `Number to low`;
-    } else {
-      message.textContent = `You lost the game`;
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#E0144C';
-    }
-  }
-});
+// const setScore = function (data) {
+//   score--;
+//   document.querySelector('.score').textContent = score;
+//   message.textContent =
+//     data > sectretNumber ? `Number to hight` : `Number to low`;
+// };
+// ///////////////////////////////////////////////////////
+
+// let sectretNumber = Math.trunc(Math.random() * 20 + 1);
+// let message = document.querySelector('.message');
+// let score = 20;
+// let higherScore = 0;
+// console.log(sectretNumber);
+// document.querySelector('.check').addEventListener('click', function () {
+//   let guess = Number(document.querySelector('.guess').value);
+
+//   if (!guess) {
+//     message.textContent = 'No number';
+//     //When player wins
+//   } else if (guess === sectretNumber) {
+//     if (score > higherScore) {
+//       higherScore = score;
+//       document.querySelector('.higherScore').textContent = higherScore;
+//     }
+//     document.querySelector('.score').textContent = score;
+//     message.textContent = `Correct Number`;
+//     document.querySelector('body').style.backgroundColor = '#60b347';
+//     document.querySelector('.number').style.width = '30rem';
+//     document.querySelector('.number').textContent = `${sectretNumber}`;
+//   }
+//   ///When guest is wrong
+//   else if (guess !== sectretNumber) {
+//     // When user us to high
+//     if (score > 1) {
+//       setScore(guess);
+//     } else {
+//       YouLost();
+//     }
+//   }
+// });
+
+// document.querySelector('.again').addEventListener('click', function () {
+//   document.querySelector('.number').textContent = `?`;
+//   score = 20;
+//   sectretNumber = Math.trunc(Math.random() * 20 + 1);
+//   console.log(sectretNumber);
+//   message.textContent = `Start guessing...`;
+//   document.querySelector('.score').textContent = score;
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   document.querySelector('.number').style.width = '15rem';
+//   document.querySelector('.guess').value = '';
+// });
